7692aae2d1b590e5105e104ee22353638291b345
Zuzanna Komendecka <z.komendecka@gmail.com>
2022-11-25 13:14:32 +0100

    add playerMove

--- js/script.js
+++ js/script.js
@@ -9,3 +9,18 @@ if (randomNumber == '1') {
     computerMove = 'nożyczki';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyczki';
+} else {
+    printMessage('Zła cyfra wydaje mi sie ze chciałeś 1');
+    playerMove = 'kamień';
+}
+printMessage('Twój ruch: ' + playerMove);
